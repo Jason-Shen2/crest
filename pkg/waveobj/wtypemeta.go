aa4439ffe7aa31141e418d7e5ccaf4fae33b9684
Jason Shen <zshen339@gatech.edu>
2026-07-04 15:08:13 +0800

    feat: create tabs with an initial block

--- pkg/waveobj/wtypemeta.go
+++ pkg/waveobj/wtypemeta.go
@@ -93,6 +93,7 @@ type MetaTSType struct {
 
 	// for tabs
 	TabFlagColor        string  `json:"tab:flagcolor,omitempty"`
+	TabAutoName         bool    `json:"tab:autoname,omitempty"`
 	TabBackground       string  `json:"tab:background,omitempty"`
 	BgClear             bool    `json:"bg:*,omitempty"`
 	Bg                  string  `json:"bg,omitempty"`
@@ -105,7 +106,7 @@ type MetaTSType struct {
 	LayoutVTabBarWidth        int   `json:"layout:vtabbarwidth,omitempty"`
 	LayoutWidgetsVisible      *bool `json:"layout:widgetsvisible,omitempty"`
 	LayoutFileExplorerVisible *bool `json:"layout:fileexplorervisible,omitempty"`
-	LayoutFileExplorerWidth int `json:"layout:fileexplorerwidth,omitempty"`
+	LayoutFileExplorerWidth   int   `json:"layout:fileexplorerwidth,omitempty"`
 
 	TermClear               bool     `json:"term:*,omitempty"`
 	TermFontSize            int      `json:"term:fontsize,omitempty"`
